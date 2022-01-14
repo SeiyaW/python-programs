f0c366137589aecd8a4587382cac44643bcd08e4
SeiyaW <seiyaprogram007@gmail.com>
2022-01-14 11:42:36 +0900

    コメントの編集と追加

--- api.py
+++ api.py
@@ -1,9 +1,15 @@
+#-*- code:utf-8 -*-
+# 作成者：SeiyaW
+#2022/01/14
+
 import requests
 
+# apiクラス
 class api():
   # 初期設定
   def __init__(self):
     self.jusyo_url = "https://zip-cloud.appspot.com/api/search"
+    # 270000.jsonは大阪の都道府県コード
     self.weather_url = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/270000.json"
 
   # 住所検索関数
@@ -15,15 +21,14 @@ class api():
         break
       print(z)
       param = {"zipcode": z}
-      data = requests.get(url, param).json()
+      data = requests.get(url, param).json()#json形式でデータを取得
       address = data["results"][0]
       print(address["address1"] + address["address2"] + address["address3"])
 
   #天気予報関数
   def weather(self):
-    # 270000.jsonは大阪の都道府県コード
     url = self.weather_url
-    data = requests.get(url).json()
+    data = requests.get(url).json()#json形式でデータを取得
     print(data['headlineText'])
     print(data['text'])
 
