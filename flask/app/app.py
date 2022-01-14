68292e02fb26824f811eb55a17570f6d54247359
SeiyaW <seiyaprogram007@gmail.com>
2022-01-14 14:04:33 +0900

    if文での制御を追加 クエリストリングを受け取ってhtmlに送る

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World"
-
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    name = request.args.get("name")
+    return render_template("index.html", name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
