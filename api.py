486743770612e8a89442da0063f81ada6f06aba5
SeiyaW <seiyaprogram007@gmail.com>
2022-01-14 10:59:25 +0900

    apiファイル　初期設定「def __init__(self)」追加しました。

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
 import requests
 
 class api():
+  # 初期設定
+  def __init__(self):
+    self.jusyo_url = "https://zip-cloud.appspot.com/api/search"
+    self.weather_url = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/270000.json"
+
   # 住所検索関数
   def jusyo(self):
-    url = "https://zip-cloud.appspot.com/api/search"
+    url = self.jusyo_url
     while True:
       z = input("7桁の郵便番号を入力してください(ハイフンは含まない): ")
       if not z:
@@ -17,7 +22,7 @@ class api():
   #天気予報関数
   def weather(self):
     # 270000.jsonは大阪の都道府県コード
-    url = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/270000.json"
+    url = self.weather_url
     data = requests.get(url).json()
     print(data['headlineText'])
     print(data['text'])
